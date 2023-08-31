560a0ae9e70193de8554daded0a9a40aa03f7fce
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-31 18:45:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def video(websocket: WebSocket):
         return {"age": 23, "gender": 'M'}  # TODO: Replace with actual data
 
 
-# For video uploads
+# For video file uploads
 @app.post("/upload/")
 async def upload_video(file: UploadFile):
     if file.content_type != "video/mp4":
