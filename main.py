7518e36efc377af6c9cde6755cb88904cb93f9aa
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-31 15:46:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Image(BaseModel):
+    image: str
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.post("/evaluate_image")
+async def evaluate_image(image: Image):
+    # Call the method that evaluates the image and returns the age and gender of the user
+    age, gender = evaluate(image.image)
+    return {"age": age, "gender": gender}
