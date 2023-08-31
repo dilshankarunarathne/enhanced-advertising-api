d0c8e8dab4232f3fd7ecbebcc25705249d8d273b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-31 18:19:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+
 class Image(BaseModel):
     image: str
 
+
 @app.post("/image")
 async def evaluate_image(image: Image):
-    age, gender = (23, 'M') # TODO: Replace with actual model
+    age, gender = (23, 'M')  # TODO: Replace with actual model
     return {"age": age, "gender": gender}
 
 
@@ -18,4 +20,4 @@ async def video(websocket: WebSocket):
     while True:
         data = await websocket.receive_bytes()
         # TODO: Process video data
-        return {"age": 23, "gender": 'M'} # TODO: Replace with actual data
+        return {"age": 23, "gender": 'M'}  # TODO: Replace with actual data
