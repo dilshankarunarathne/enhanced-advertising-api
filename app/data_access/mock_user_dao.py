95ae39ff609e2d04528732d6a0f789aad500ac21
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-01 19:12:28 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -72,4 +72,4 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     user = UserInDB(**user_dict)
     hashed_password = mock_hash_password(form_data.password)
-    
+    if not hashed_password == user.hashed_password:
