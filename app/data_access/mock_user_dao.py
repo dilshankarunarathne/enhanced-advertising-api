d35182909c6ca2548b93dca5628b5be90dbfd6e5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-01 18:55:32 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -16,6 +16,8 @@ fake_users_db = {
 }
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
 # replace "token" with /token endpoint, this is where a client should obtain a token from
 
 def get_user(db, username: str):
