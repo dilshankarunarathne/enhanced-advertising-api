f1263e7aa128f3c49f12189a229d309ecd8784d1
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-01 19:13:53 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -21,7 +21,7 @@ fake_users_db = {
     },
 }
 
-
+def fake_hash_password(password: str):
 
 # replace "token" with '/token' endpoint, this is where a client should obtain a token from.
 # this is the endpoint that the client will send the username and password to, and if they are valid,
