5388e0ec93acbb4cac175cb04f064df3e9d355bc
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-01 18:56:35 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -28,7 +28,5 @@ def get_user(db, username: str):
 
 
 def fake_decode_token(token):
-    # This doesn't provide any security at all
-    # Check the next version
     user = get_user(fake_users_db, token)
     return user
