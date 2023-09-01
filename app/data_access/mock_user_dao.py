b38ace26743984b3b139c5950ae7b1352ee7507b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-01 19:00:37 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -18,6 +18,7 @@ fake_users_db = {
 }
 
 # replace "token" with '/token' endpoint, this is where a client should obtain a token from.
+# this is the endpoint that the client will send the username and password to, and if they are valid,
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
