9528aac2f2413c6f2f42d8ae48bc53bb0695b5de
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-01 21:52:53 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -98,4 +98,5 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
 
 @router.get("/")
 async def auth_root(current_user: Annotated[User, Depends(get_current_user)]):
-    return {"message": "Hello World"}
+    if current_user.is_adviser:
+        return {"message": "Hello Adviser"}
