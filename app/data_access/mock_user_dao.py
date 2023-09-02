cb4ca3746abdb434322e741980074afd4d7892b9
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 08:50:08 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -73,6 +73,7 @@ def get_user(db, username: str):
         return UserInDB(**user_dict)
 
 
+
 def authenticate_user(fake_db, username: str, password: str):
     user = get_user(fake_db, username)
     if not user:
