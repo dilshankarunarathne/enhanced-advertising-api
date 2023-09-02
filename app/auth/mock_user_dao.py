fe7efd26168894965976c05bb93e674fc6cf64b4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:30:32 +0530

    Update mock_user_dao.py

--- app/auth/mock_user_dao.py
+++ app/auth/mock_user_dao.py
@@ -1,15 +1,15 @@
-from datetime import timedelta
-from typing import Annotated
-
-from fastapi import HTTPException, status, Depends, APIRouter, Form
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt, JWTError
-from passlib.context import CryptContext
-
-from app.auth.models.token_model import TokenData
-from app.auth.models.user_model import UserInDB, User
-from app.auth.user_dao import get_user
-from app.security.hashing import verify_password, get_password_hash, create_access_token, is_token_blacklisted
+# from datetime import timedelta
+# from typing import Annotated
+#
+# from fastapi import HTTPException, status, Depends, APIRouter, Form
+# from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+# from jose import jwt, JWTError
+# from passlib.context import CryptContext
+#
+# from app.auth.models.token_model import TokenData
+# from app.auth.models.user_model import UserInDB, User
+# from app.auth.user_dao import get_user
+# from app.security.hashing import verify_password, get_password_hash, create_access_token, is_token_blacklisted
 
 # to get a string like this run:
 # openssl rand -hex 32
