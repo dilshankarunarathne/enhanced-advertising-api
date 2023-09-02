284e44836a55286a178ca9678e4f4cbd838aee32
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:27:00 +0530

    Update auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -22,7 +22,7 @@ router = APIRouter(
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/auth/token")
 
 
-def authenticate_user(fake_db, username: str, password: str):
+def authenticate_user(username: str, password: str):
     user = get_user(fake_db, username)
     if not user:
         return False
