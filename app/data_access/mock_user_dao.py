ff3a1c9df238e55f42762a01edc61348d716c775
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 08:50:18 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -75,6 +75,7 @@ def get_user(db, username: str):
 
 # this method is used to authenticate a user.
 # method should be able to get the username and password from the request body and check in the db to see if
+
 def authenticate_user(fake_db, username: str, password: str):
     user = get_user(fake_db, username)
     if not user:
