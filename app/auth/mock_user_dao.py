fb952c142638b7f1c5b34bd6abef95c467aacf31
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:30:23 +0530

    Update mock_user_dao.py

--- app/auth/mock_user_dao.py
+++ app/auth/mock_user_dao.py
@@ -18,22 +18,22 @@ SECRET_KEY = "3ac975a63c346504ccc4bad65505c619a2cc502b01c7a92e3288f4d3b0def92b"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-fake_users_db = {
-    "johndoe": {
-        "id": 1,
-        "username": "johndoe",
-        "email": "johndoe@example.com",
-        "hashed_password": "fake-hashed-secret",
-        "is_adviser": False,
-    },
-    "alice": {
-        "id": 2,
-        "username": "alice",
-        "email": "alice@example.com",
-        "hashed_password": "fake-hashed-secret-2",
-        "is_adviser": True,
-    },
-}
+# fake_users_db = {
+#     "johndoe": {
+#         "id": 1,
+#         "username": "johndoe",
+#         "email": "johndoe@example.com",
+#         "hashed_password": "fake-hashed-secret",
+#         "is_adviser": False,
+#     },
+#     "alice": {
+#         "id": 2,
+#         "username": "alice",
+#         "email": "alice@example.com",
+#         "hashed_password": "fake-hashed-secret-2",
+#         "is_adviser": True,
+#     },
+# }
 
 # fake_blacklist = []
 
