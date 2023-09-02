24d638d96775b39aff9e19ddbf9e9b560214793f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:06:30 +0530

    Update user_dao.py

--- app/data_access/user_dao.py
+++ app/data_access/user_dao.py
@@ -1,10 +1,5 @@
 from fastapi import APIRouter
 from fastapi.security import OAuth2PasswordBearer
 
-router = APIRouter(
-    prefix="/auth",
-    tags=["auth"],
-    responses={404: {"description": "Not found"}},
-)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/auth/token")
