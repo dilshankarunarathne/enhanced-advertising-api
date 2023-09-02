02bf595f9185f28d7b87c06dd4a5ef7ac2d62ead
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 11:05:27 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -188,13 +188,6 @@ class RegistrationForm(BaseModel):
     password: str
     is_adviser: bool
 
-    def as_form(self) -> dict:
-        return {
-            "username": self.username,
-            "email": self.email,
-            "password": self.password,
-            "is_adviser": self.is_adviser,
-        }
 
 
 @router.post("/register")
