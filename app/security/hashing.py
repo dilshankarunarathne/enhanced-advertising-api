193c30d5531ac7e7bb32c721153437c77c700325
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:53:24 +0530

    Update hashing.py

--- app/security/hashing.py
+++ app/security/hashing.py
@@ -1,7 +1,8 @@
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 
-
+SECRET_KEY = "3ac975a63c346504ccc4bad65505c619a2cc502b01c7a92e3288f4d3b0def92b"
+ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
