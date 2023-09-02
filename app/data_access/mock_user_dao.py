30e51446ddf257e3a3f189796dab11351e66ac8e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 09:08:58 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -118,6 +118,7 @@ def fake_decode_token(token):
 # this method is used to get the current user.
 # method should be able to get the token from the request header and check in the db to see if
 # an actual user exists with the username that is encoded in the token. If the user exists,
+
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
