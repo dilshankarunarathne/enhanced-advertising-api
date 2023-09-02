08e868a9281c6c849fbedd61fe8470374c1f8ac8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:55:02 +0530

    Update hashing.py

--- app/security/hashing.py
+++ app/security/hashing.py
@@ -12,8 +12,11 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+
 def get_password_hash(password):
     return pwd_context.hash(password)
+
 
 def create_access_token(data: dict, expires_delta: int = None):
     to_encode = data.copy()
