9c6c2de3872102c8636be14196ec9df8da1e27a0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 09:01:33 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -116,6 +116,10 @@ def fake_decode_token(token):
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid authentication credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
 
 
 # tested - working...! :)
