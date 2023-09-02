5cc42bcd410c6d8f371a2913ea1630ee582f6832
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 09:02:17 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -120,6 +120,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    
 
 
 # tested - working...! :)
