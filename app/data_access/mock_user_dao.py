1fd2fa669e83f3fc41fe77e631751f848e151dde
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 11:31:07 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -183,6 +183,7 @@ async def auth_root(current_user: Annotated[User, Depends(get_current_user)]):
 
 #############################################################################################
 
+# this method is used by the users to register.
 @router.post("/register")
 async def register_user(
         username: str = Form(...),
