08b0f3475bd3bbaef75f6f05c43db20495150a45
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 09:04:39 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -6,6 +6,7 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from passlib.context import CryptContext
 
+from app.data_access.model.token_dao import TokenData
 from app.data_access.model.user_model import UserInDB, User
 
 # to get a string like this run:
