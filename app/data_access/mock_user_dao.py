e7023977ba9cc145a123863b83a57e47f4d74ca4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:40:54 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -128,6 +128,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+    
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
