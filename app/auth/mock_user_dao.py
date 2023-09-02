dd8976b1fbaded408eb7fa99157d23472b5bb642
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:29:06 +0530

    Update mock_user_dao.py

--- app/auth/mock_user_dao.py
+++ app/auth/mock_user_dao.py
@@ -154,11 +154,11 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
 
 # this method is used to add a token to the blacklist. This method will be used by the server to blacklist a token.
 # method should be able to get the token from the request header and add it to the blacklist.
-@router.post("/logout")
-async def logout(token: str = Depends(oauth2_scheme)):
-    # ad the token to the blacklist
-    fake_blacklist.append(token)
-    return {"message": "Successfully logged out"}
+# @router.post("/logout")
+# async def logout(token: str = Depends(oauth2_scheme)):
+#     # ad the token to the blacklist
+#     fake_blacklist.append(token)
+#     return {"message": "Successfully logged out"}
 
 
 # tested - working...! :)
