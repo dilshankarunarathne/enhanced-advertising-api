3080158a8e91cdfdfed2717dd662872564dcd06d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:19:34 +0530

    Update user_dao.py

--- app/auth/user_dao.py
+++ app/auth/user_dao.py
@@ -20,7 +20,7 @@ def get_next_avail_id() -> int:
     pass
 
 
-# check if the user exists in the db and if so, 
+# check if the user exists in the db and if so, construct and return a UserInDB
 def get_user(db, username: str):
     # TODO: implement this method
     pass
