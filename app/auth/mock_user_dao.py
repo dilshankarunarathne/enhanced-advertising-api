f9a2c5b9d3b8d9c367610a1acfd6fab2e0cf5b7d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:30:05 +0530

    Update mock_user_dao.py

--- app/auth/mock_user_dao.py
+++ app/auth/mock_user_dao.py
@@ -130,26 +130,26 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # an actual user exists with the username that is encoded in the token. If the user exists,
 # the method should return the user, otherwise it should raise an exception.
 # here, we are mocking the token with just by the username and the db with a dictionary
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    if is_token_blacklisted(token):  # updated for logout mechanism
-        raise credentials_exception
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except JWTError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
+# async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     if is_token_blacklisted(token):  # updated for logout mechanism
+#         raise credentials_exception
+#     try:
+#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+#         username: str = payload.get("sub")
+#         if username is None:
+#             raise credentials_exception
+#         token_data = TokenData(username=username)
+#     except JWTError:
+#         raise credentials_exception
+#     user = get_user(fake_users_db, username=token_data.username)
+#     if user is None:
+#         raise credentials_exception
+#     return user
 
 
 # this method is used to add a token to the blacklist. This method will be used by the server to blacklist a token.
