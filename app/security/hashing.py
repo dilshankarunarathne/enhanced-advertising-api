93146f32c98c9b896c95b689710156aaf8aac3d8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:52:19 +0530

    Update hashing.py

--- app/security/hashing.py
+++ app/security/hashing.py
@@ -4,3 +4,14 @@ def decode_token(token):
 
 def hash_password(password: str):
     pass  # TODO: implement this method
+
+
+def create_access_token(data: dict, expires_delta: int = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)  # changes were made here
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
