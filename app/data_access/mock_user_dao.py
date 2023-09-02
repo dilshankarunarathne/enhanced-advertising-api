356424bea3481f078afab7912d67d0eef400b9f8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 08:40:43 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -10,6 +10,7 @@ from app.data_access.model.user_model import UserInDB, User
 # this is the secret key that will be used to sign the JWT token
 SECRET_KEY = "3ac975a63c346504ccc4bad65505c619a2cc502b01c7a92e3288f4d3b0def92b"
 ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 fake_users_db = {
     "johndoe": {
