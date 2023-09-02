4ae289e1d43d1f6ba2529cbfe56164c8a5c56792
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:54:47 +0530

    Update hashing.py

--- app/security/hashing.py
+++ app/security/hashing.py
@@ -9,7 +9,8 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: int = None):
     to_encode = data.copy()
