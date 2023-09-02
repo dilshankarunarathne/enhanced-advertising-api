69905b4ac643a3df309a09a3847eebcc0c0b20d5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:58:05 +0530

    Update hashing.py

--- app/security/hashing.py
+++ app/security/hashing.py
@@ -27,3 +27,6 @@ def create_access_token(data: dict, expires_delta: int = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def is_token_blacklisted(token):
+    return token in fake_blacklist
