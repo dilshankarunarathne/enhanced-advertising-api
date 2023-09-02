406306f0760158a952ab334330e6e3bf426c94c4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 09:01:30 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -115,7 +115,7 @@ def fake_decode_token(token):
 
 
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    
+    credentials_exception = HTTPException(
 
 
 # tested - working...! :)
