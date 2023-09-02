2956d69ebd6fa47e1502066a5c8c8a2e0fd26b50
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:07:06 +0530

    Update hashing.py

--- app/security/hashing.py
+++ app/security/hashing.py
@@ -3,6 +3,10 @@ from datetime import datetime, timedelta
 from jose import jwt
 from passlib.context import CryptContext
 
+"""
+    middleware for hashing passwords and creating tokens
+"""
+
 SECRET_KEY = "3ac975a63c346504ccc4bad65505c619a2cc502b01c7a92e3288f4d3b0def92b"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
