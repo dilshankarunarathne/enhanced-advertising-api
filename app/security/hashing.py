be9d0223111988de88ea34c19a01f0c55bd09c71
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:54:32 +0530

    Update hashing.py

--- app/security/hashing.py
+++ app/security/hashing.py
@@ -6,6 +6,7 @@ SECRET_KEY = "3ac975a63c346504ccc4bad65505c619a2cc502b01c7a92e3288f4d3b0def92b"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 
