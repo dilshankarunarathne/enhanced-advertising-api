38233306ab171b2ed5a7c100daed1ad051299c6e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 09:11:45 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -149,7 +149,21 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
 async def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
 ):
-    
+    user = authenticate_user(
+        fake_users_db, form_data.username, form_data.password
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    # this is the token that will be returned to the client
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires.seconds
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 # endpoint for testing purposes
