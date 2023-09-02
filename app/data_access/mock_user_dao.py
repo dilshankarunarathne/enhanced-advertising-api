90e3aed0b0d849cf5e9e53413e9b980dcee34c1d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:55:27 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -8,7 +8,7 @@ from passlib.context import CryptContext
 
 from app.data_access.model.token_model import TokenData
 from app.data_access.model.user_model import UserInDB, User
-from app.security.hashing import verify_password
+from app.security.hashing import verify_password, get_password_hash
 
 # to get a string like this run:
 # openssl rand -hex 32
