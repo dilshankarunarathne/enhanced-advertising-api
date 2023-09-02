a5a7fa2099306c434bdb42721e83dc55a7202fef
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 14:44:15 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -153,7 +153,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
 
 
 
-# 
+# this method is used to get the current active user.
 @router.post("/logout")
 async def logout(token: str = Depends(oauth2_scheme)):
     # ad the token to the blacklist
