cea21fe622129bdd0a0cd36f39486b35982696d5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:31:02 +0530

    Update auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -49,7 +49,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+    user = get_user(username=token_data.username)
     if user is None:
         raise credentials_exception
     return user
