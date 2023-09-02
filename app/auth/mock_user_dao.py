577d1aa581c16f322a94fde1b35b5bd93f53854e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:23:31 +0530

    Update mock_user_dao.py

--- app/auth/mock_user_dao.py
+++ app/auth/mock_user_dao.py
@@ -166,25 +166,25 @@ async def logout(token: str = Depends(oauth2_scheme)):
 # method should be able to get the username and password from the request body and check in the db to see if
 # an actual user exists with the username and password that are passed in. If the user exists,
 # the method should return the user, otherwise it should raise an exception.
-@router.post("/token")
-async def login_for_access_token(
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
-):
-    user = authenticate_user(
-        fake_users_db, form_data.username, form_data.password
-    )
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    # this is the token that will be returned to the client
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires.seconds
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
+# @router.post("/token")
+# async def login_for_access_token(
+#         form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+# ):
+#     user = authenticate_user(
+#         fake_users_db, form_data.username, form_data.password
+#     )
+#     if not user:
+#         raise HTTPException(
+#             status_code=status.HTTP_401_UNAUTHORIZED,
+#             detail="Incorrect username or password",
+#             headers={"WWW-Authenticate": "Bearer"},
+#         )
+#     # this is the token that will be returned to the client
+#     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+#     access_token = create_access_token(
+#         data={"sub": user.username}, expires_delta=access_token_expires.seconds
+#     )
+#     return {"access_token": access_token, "token_type": "bearer"}
 
 
 # endpoint for testing purposes
