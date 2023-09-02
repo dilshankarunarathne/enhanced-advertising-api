ad805122ff89be2bbe16c21a05757300171978fd
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:29:37 +0530

    Update auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -31,7 +31,26 @@ def authenticate_user(username: str, password: str):
     return user
 
 
-
+async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    if is_token_blacklisted(token):  # updated for logout mechanism
+        raise credentials_exception
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(fake_users_db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 
 @router.post("/register")
