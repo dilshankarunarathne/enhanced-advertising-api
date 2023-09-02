459f8900e09634c85ed9474304ca15f21b1425ac
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:16:48 +0530

    Update auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Form, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
 from app.auth.models.user_model import UserInDB
-from app.auth.user_dao import user_exists, get_next_avail_id
+from app.auth.user_dao import user_exists, get_next_avail_id, add_new_user
 from app.security.hashing import get_password_hash
 
 """
