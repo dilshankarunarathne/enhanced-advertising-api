31367d2b3bee88925e031666170b8d97d984e83e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 08:57:59 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -96,7 +96,7 @@ def create_access_token(data: dict, expires_delta: int = None):
         expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)  # changes were made here
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    
+    return encoded_jwt
 
 
 # this method is used to verify the token.
