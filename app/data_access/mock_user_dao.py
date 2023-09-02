52ebbfa864f9df065a38380a04d2f7bfbb3e9b83
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 09:10:16 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -147,7 +147,8 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
 # the method should return the user, otherwise it should raise an exception.
 @router.post("/token")
 async def login_for_access_token(
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+        form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+):
     user_dict = fake_users_db.get(form_data.username)
     if not user_dict:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
