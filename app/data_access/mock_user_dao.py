717a4d8a636a4432088d96bd4cc21ad1096ea478
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 10:24:17 +0530

    Update mock_user_dao.py

--- app/data_access/mock_user_dao.py
+++ app/data_access/mock_user_dao.py
@@ -180,4 +180,4 @@ async def auth_root(current_user: Annotated[User, Depends(get_current_user)]):
 
 #############################################################################################
 
-@router
+@router.post("/register")
