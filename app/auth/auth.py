f77466b0378f5fbbf7607ebf5f3c526eb71182f4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-02 15:10:54 +0530

    Update auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -12,3 +12,27 @@ router = APIRouter(
 )
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/auth/token")
+
+
+@router.post("/register")
+async def register_user(
+        username: str = Form(...),
+        email: str = Form(...),
+        password: str = Form(...),
+        is_adviser: bool = Form(...),
+):
+    if username in fake_users_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Username already exists",
+        )
+    hashed_password = get_password_hash(password)
+    user = UserInDB(
+        id=len(fake_users_db) + 1,
+        username=username,
+        email=email,
+        hashed_password=hashed_password,
+        is_adviser=is_adviser,
+    )
+    fake_users_db[username] = user.model_dump()
+    return user
