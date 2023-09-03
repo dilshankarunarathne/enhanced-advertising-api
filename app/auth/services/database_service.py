09bd7067c5198e91a67d4446407ae0cb4def1081
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-03 09:24:47 +0530

    Update database_service.py

--- app/auth/services/database_service.py
+++ app/auth/services/database_service.py
@@ -1,5 +1,5 @@
 dao = EnAdDAO(host="localhost", user="root", password="", database="enad")
 try:
-    user_dao.connect()
+    dao.connect()
 except Exception as e:
     print("User DB connection error:", e)
