750fa09413ce500ecbbeeaa6172eb892632480c1
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-03 14:09:32 +0530

    Update auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -25,13 +25,7 @@ router = APIRouter(
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/auth/login")
 
 
-def authenticate_user(username: str, password: str):
-    user = get_user(username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
+
 
 
 
