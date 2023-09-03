855b53208ea7112b14aacfd4d002750ad6814bb1
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-03 09:53:42 +0530

    Update auth.py

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -95,7 +95,7 @@ async def login_for_access_token(
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": user.username}, expires_delta=access_token_expires.seconds
-        # Unresolved attribute reference 'username' for class 'bool' 
+        # Unresolved attribute reference 'username' for class 'bool'
     )
     return {"access_token": access_token, "token_type": "bearer"}
 
