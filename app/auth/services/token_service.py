acc4d653b2dc986d844065ddb4729c929b61112b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-03 09:34:19 +0530

    Update token_service.py

--- app/auth/services/token_service.py
+++ app/auth/services/token_service.py
@@ -2,7 +2,7 @@ from app.auth.services.database_service import dao
 
 
 def add_token_to_blacklist(token: str):
-    dao.
+    dao.blacklist_token
 
 
 def check_if_token_is_blacklisted(token: str) -> bool:
