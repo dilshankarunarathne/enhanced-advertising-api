35c0c51ccc641c973d31f56acd4a97b904bf1e61
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-03 14:03:24 +0530

    Update image.py

--- app/routers/image.py
+++ app/routers/image.py
@@ -9,6 +9,7 @@ router = APIRouter(
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/auth/login")
 
+
 @router.post("/")
 async def evaluate_image(
         image: UploadFile = File(...),
