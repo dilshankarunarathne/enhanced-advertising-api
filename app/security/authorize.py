0ffe89d4f195720c61b02cd78c0d09245abf4450
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 11:24:16 +0530

    Update authorize.py

--- app/security/authorize.py
+++ app/security/authorize.py
@@ -12,6 +12,8 @@ from app.security.hashing import is_token_blacklisted, SECRET_KEY, ALGORITHM, ve
     authorization function middleware
 """
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="http://127.0.0.1:8000/auth/login")
+
 credentials_exception = HTTPException(
     status_code=status.HTTP_401_UNAUTHORIZED,
     detail="Could not validate credentials",
