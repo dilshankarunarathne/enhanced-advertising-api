df86a26e2b0af72da4786ee5edb4b20877e13bf3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-09-04 12:46:11 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
